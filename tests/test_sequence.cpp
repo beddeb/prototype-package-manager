470d951fab6c35085eb75caefafea0c900a52ce5
Дмитрий Заранкович <DZarankovich@yandex.ru>
2024-12-10 23:53:50 +0300

    test0

---
+++ tests/test_sequence.cpp
@@ -0,0 +1,145 @@
+#include <cassert>
+#include <iostream>
+#include "test_sequence.hpp"
+
+
+void testAddElement() {
+    Sequence<int> seq;
+    seq.add(10);
+    assert(("AddElement_size", seq.getSize() == 1));
+    assert(("AddElement_value", seq.get(0) == 10));
+}
+
+void testAddMultipleElements() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq.add(20);
+    seq.add(30);
+    assert(("AddMultipleElements_size", seq.getSize() == 3));
+    assert(("AddMultipleElements_value_0", seq.get(0) == 10));
+    assert(("AddMultipleElements_value_1", seq.get(1) == 20));
+    assert(("AddMultipleElements_value_2", seq.get(2) == 30));
+}
+
+void testRemoveElement() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq.add(20);
+    seq.add(30);
+    seq.remove(1);
+    assert(("RemoveElement_size", seq.getSize() == 2));
+    assert(("RemoveElement_value_0", seq.get(0) == 10));
+    assert(("RemoveElement_value_1", seq.get(1) == 30));
+}
+
+void testRemoveOutOfRange() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq.add(20);
+    try {
+        seq.remove(2);
+        assert(("RemoveOutOfRange_remove", false));
+    } catch (const std::out_of_range&) {
+        assert(("RemoveOutOfRange_OK", true));
+    }
+}
+
+void testGetOutOfRange() {
+    Sequence<int> seq;
+    seq.add(10);
+    try {
+        seq.get(1);
+        assert(("GetOutOfRange_value", false));
+    } catch (const std::out_of_range&) {
+        assert(("GetOutOfRange_OK", true));
+    }
+}
+
+void testCopyConstructor() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq.add(20);
+    Sequence<int> copy(seq);
+    assert(("CopyConstructor_size", copy.getSize() == 2));
+    assert(("CopyConstructor_value_0", copy.get(0) == 10));
+    assert(("CopyConstructor_value_1", copy.get(1) == 20));
+}
+
+void testMoveConstructor() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq.add(20);
+    Sequence<int> moved(std::move(seq));
+    assert(("MoveConstructor_size_0", moved.getSize() == 2));
+    assert(("MoveConstructor_value_0", moved.get(0) == 10));
+    assert(("MoveConstructor_value_1", moved.get(1) == 20));
+    assert(("MoveConstructor_size_1", seq.getSize() == 0));
+}
+
+void testAssignmentOperator() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq.add(20);
+    Sequence<int> copy;
+    copy = seq;
+    assert(("SelfAssignment_size", copy.getSize() == 2));
+    assert(("SelfAssignment_value_0", copy.get(0) == 10));
+    assert(("SelfAssignment_value_1", copy.get(1) == 20));
+}
+
+void testSelfAssignment() {
+    Sequence<int> seq;
+    seq.add(10);
+    seq = seq;
+    assert(("SelfAssignment_size", seq.getSize() == 1));
+    assert(("SelfAssignment_value", seq.get(0) == 10));
+}
+
+void testEqualityOperator() {
+    Sequence<int> seq1;
+    seq1.add(10);
+    seq1.add(20);
+
+    Sequence<int> seq2;
+    seq2.add(10);
+    seq2.add(20);
+
+    assert(("EqualityOperator", seq1 == seq2));
+}
+
+void testInequalityOperator() {
+    Sequence<int> seq1;
+    seq1.add(10);
+
+    Sequence<int> seq2;
+    seq2.add(20);
+
+    assert(("InequalityOperator", seq1 != seq2));
+}
+
+void testResize() {
+    Sequence<int> seq;
+    for (int i = 0; i < 10; ++i) {
+        seq.add(i);
+    }
+    assert(seq.getSize() == 10);
+    for (int i = 0; i < 10; ++i) {
+        assert(("Resize", seq.get(i) == i));
+    }
+}
+
+void testSequence() {
+    testAddElement();
+    testAddMultipleElements();
+    testRemoveElement();
+    testRemoveOutOfRange();
+    testGetOutOfRange();
+    testCopyConstructor();
+    testMoveConstructor();
+    testAssignmentOperator();
+    testSelfAssignment();
+    testEqualityOperator();
+    testInequalityOperator();
+    testResize();
+    std::cout << "All tests passed!" << std::endl;
+}
---
+++ tests/test_sequence.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../include/sequence.hpp"
+
+
+void testAddElement();
+
+void testAddMultipleElements();
+
+void testRemoveElement();
+
+void testRemoveOutOfRange();
+
+void testGetOutOfRange();
+
+void testCopyConstructor();
+
+void testMoveConstructor();
+
+void testAssignmentOperator();
+
+void testSelfAssignment();
+
+void testEqualityOperator();
+
+void testInequalityOperator();
+
+void testResize();
+
+void testSequence();
